24d2265afd42d095dc3fe8b78039c92d85121c57
IsabelVallejoUam <isabel.vallejog@autonoma.edu.co>
2022-09-07 18:55:23 -0500

    se intento

---
+++ src/screens/Camerascreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { StyleSheet, View, Button, Alert, Text } from 'react-native'
+import { Camera, CameraType } from 'react-native-camera-kit'
+
+import App from '../../App'
+
+export const Camerascreen = () => {
+  return (
+    <Text>AYUDA</Text>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#fff'
+    }
+})
+
+
+
